60572b334967faaf834eda25e019a2315b261ac3
thillith <lilithrennie@gmail.com>
2026-04-24 00:10:56 +0000

    Added history to music from last.fm :3 (very jank) (Not dun yet)

--- lastfm.js
+++ lastfm.js
@@ -15,6 +15,11 @@ async function loadLastFM() {
     const data = JSON.parse(text);
 
     const track = data?.recenttracks?.track?.[0];
+    const track1 = data?.recenttracks?.track?.[1];
+    const track2 = data?.recenttracks?.track?.[2];
+    const track3 = data?.recenttracks?.track?.[3];
+    const track4 = data?.recenttracks?.track?.[4];
+    
 
     if (!track) {
       output.innerHTML = "No track found.";
@@ -26,15 +31,67 @@ async function loadLastFM() {
     const image = track.image?.[3]?.["#text"] ?? "";
     const nowPlaying = track["@attr"]?.nowplaying === "true";
 
+    const artist1 = track1?.artist?.["#text"] ?? "Unknown Artist";
+    const name1 = track1?.name ?? "Unknown Track";
+    const image1 = track1?.image?.[1]?.["#text"] ?? "";
+
+    const artist2 = track2?.artist?.["#text"] ?? "Unknown Artist";
+    const name2 = track2?.name ?? "Unknown Track";
+    const image2 = track2?.image?.[1]?.["#text"] ?? "";
+
+    const artist3 = track3?.artist?.["#text"] ?? "Unknown Artist";
+    const name3 = track3?.name ?? "Unknown Track";
+    const image3 = track3?.image?.[1]?.["#text"] ?? "";
+
+    const artist4 = track4?.artist?.["#text"] ?? "Unknown Artist";
+    const name4 = track4?.name ?? "Unknown Track";
+    const image4 = track4.image?.[1]?.["#text"] ?? "";
+
     output.innerHTML = `
+		<div class="music-grids">
+    <div class="music-now">
       <div style="display: flex; align-items: center; gap: 12px; margin: 1% ">
-        ${image ? `<img src="${image}" width="20%" height="20%">` : ""}
+        ${image ? `<img src="${image}" width="25%">` : ""}
+
         <div>
           <div>${nowPlaying ? "Listening to:" : "Last Listened to:"}</div>
+        
           <div> ${name}</div> <b>${artist}</b>
+
         </div>
-      </div>
+        </div>
+    </div>
+    
+    <div class="music-history">
+    <div class="music-entry">
+      <img src="${image1}"
+      <div class="music-text">
+      ${name1} - <br>
+      ${artist1}
+    </div>
+      <div class="music-entry">
+      <img src="${image2}"
+      <div class="music-text">
+      ${name2} - <br>
+      ${artist2}
+    </div>
+      <div class="music-entry">
+      <img src="${image3}"
+      <div class="music-text">
+      ${name3} - <br>
+      ${artist3}
+    </div>
+      <div class="music-entry">
+      <img src="${image4}"
+      <div class="music-text">
+      ${name4} - <br>
+      ${artist4}
+    </div>
+		</div>
+		</div>
     `;
+
+  // ${image1 ? `<img src="${image1}" width="32px" height="32px">` : ""} ${name1} - <br> <b>${artist1}</b> 
 
   } catch (err) {
     console.error(err);
