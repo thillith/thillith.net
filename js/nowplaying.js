98ef4f6ae40244cbfdb60162b85e1d2b0611306c
thillith <lilithrennie@gmail.com>
2026-08-18 03:28:04 +0100

    Added live listening to the / page from last.fm

---
+++ js/nowplaying.js
@@ -0,0 +1,48 @@
+async function loadxmr() {
+  const output = document.getElementById("nowplaying");
+
+  try {
+    const res = await fetch("https://www.thillith.net/php/lastfmapi.php?user=thillith");
+
+    if (!res.ok) {
+      throw new Error("HTTP " + res.status);
+    }
+
+    const text = await res.text();
+    const data = JSON.parse(text);
+
+    const track = data?.recenttracks?.track?.[0];
+
+    const artist = track.artist?.["#text"] ?? "Unknown Artist";
+    const name = track.name ?? "Unknown Track";
+    const image = track.image?.[3]?.["#text"] ?? "";
+    const nowPlaying = track["@attr"]?.nowplaying === "true";
+    const url = track.url;
+    const artisturl = track.url.split('/_/')[0];
+
+    //Idk how this one works lol, I found it...
+    const albumurl = `https://last.fm/music/${encodeURIComponent(track.artist["#text"])}/${encodeURIComponent(track.album["#text"])}`;
+
+
+
+
+    output.innerHTML = `
+        <div class="container">
+
+            <div><p>&gt;<a href="https://last.fm/user/thillith">${nowPlaying ? "Listening to:" : "Last Listened to:"}</a> <a href=${url}>${name} - <a href=${artisturl}><b>${artist}</b></a></p></div>
+
+            <div style="display: flex; flex-wrap: wrap; flex-basis: content; align-items: flex-end;
+            padding: 4px; background-color: #00000020; border-radius: 12px;"><a href=${albumurl}>${image ? `<img src="${image}" width="96px" height="96">` : ""}</a></div>
+            </div>
+
+        </div>
+    `;
+
+  } catch (err) {
+    console.error(err);
+    output.innerHTML = "Failed to load playing:";
+  }
+}
+
+loadxmr();
+setInterval(loadxmr, 15000);
